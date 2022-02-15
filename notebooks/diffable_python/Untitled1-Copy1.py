322bb49164e05602e4fa9230b2e47a48b6b84ba8
Richard Croker <ra.croker@gmail.com>
2022-02-15 12:50:00 +0000

    new analysis created

--- notebooks/diffable_python/Untitled1-Copy1.py
+++ notebooks/diffable_python/Untitled1-Copy1.py
@@ -24,10 +24,13 @@ import matplotlib.gridspec as gridspec
 from IPython.display import display, HTML
 
 sql='''
-select distinct bnf.presentation, presc.bnf_code, dose_per_unit, new_ome_multiplier, ome_dose
-from richard.opioid_converter as opioid
-inner join 
-`ebmdatalab.richard.all_bnf_rx` as presc
+SELECT
+ presc.bnf_name, presc.bnf_code, sum(quantity) as quantity,
+  SUM(quantity*dose_per_unit*ome_multiplier) AS total_ome
+FROM
+  ebmdatalab.hscic.normalised_prescribing AS presc
+JOIN
+  ebmdatalab.richard.opioid_measure as opioid
 ON CONCAT(
     SUBSTR(presc.bnf_code,0,9),
     'AA',
@@ -36,116 +39,141 @@ ON CONCAT(
     SUBSTR(opioid.bnf_code,0,11),
     SUBSTR(opioid.bnf_code,-2,2)
   )
-inner join 
-hscic.bnf as bnf
-ON
-presc.bnf_code = bnf.presentation_code
+  where presc.month between '2020-04-01' and '2021-03-01'
+GROUP BY
+bnf_name, bnf_code
 '''
-df_opioid_old = bq.cached_read(sql, csv_path=os.path.join('..','data','df_opioid_old_test.csv'))
+exportfile = os.path.join("..","data","df_opioid_old.csv") #set path for data cache
+df_opioid_old = bq.cached_read(sql, csv_path=exportfile, use_cache=False) #save dataframe to csv
 
 df_opioid_old.head()
 
 sql='''
+#subquery to create a "simple" administration route. 
 WITH 
+simp_form AS ( 
+SELECT DISTINCT vmp, #vmp code
+                CASE 
+                    WHEN descr LIKE '%injection%' THEN 'injection' #creates "injection" as route, regardless of whether injection or infusion. this also removes injection routes, e.g.
+                    WHEN descr LIKE '%infusion%' THEN 'injection'  #s/c, i/v etc, AS often injections have many licensed routes, which would multiply the row
+                    ELSE SUBSTR(form.descr, STRPOS(form.descr,".")+1) #takes the dosage form out of the string (e.g. tablet.oral) TO leave route.
+                END AS simple_form 
+FROM dmd.ont AS ont #the coded route for dosage form, includes vmp code 
+INNER JOIN dmd.ontformroute AS form ON form.cd=ont.form #text description of route 
+  )
+
+#subquery to normalise strength to mg
+,norm_vpi AS (
+SELECT vmp, #vmp code
+       ing, #ing code
+       strnt_nmrtr_val, #numerator strength value
+       strnt_nmrtr_uom, #numerator unit of measurement
+       unit_num.descr AS unit_num, #numerator unit 
+       unit_den.descr AS unit_den, #denominator unit
+       CASE
+           WHEN unit_num.descr = 'microgram' THEN vpi.strnt_nmrtr_val / 1000 #creates miligram value from mcg value
+           WHEN unit_num.descr = 'gram' THEN vpi.strnt_nmrtr_val * 1000 #creates miligram value from gram value
+           ELSE vpi.strnt_nmrtr_val
+       END AS strnt_nmrtr_val_mg, #all listed drugs now in miligram rather than g or mcg
+       CASE
+           WHEN unit_den.descr = 'litre' THEN vpi.strnt_dnmtr_val * 1000 #some denominators listed as litre, so create mililitre value
+           ELSE vpi.strnt_dnmtr_val
+       END AS strnt_dnmtr_val_ml #denominator now in ml
+FROM dmd.vpi AS vpi
+LEFT JOIN dmd.unitofmeasure AS unit_num ON vpi.strnt_nmrtr_uom = unit_num.cd #join to create text value for numerator unit
+LEFT JOIN dmd.unitofmeasure AS unit_den ON vpi.strnt_dnmtr_uom = unit_den.cd) #join to create text value for denominator unit
+
+#subquery to create single BNF table for AMPs and VMPs commented out to check bnf_codes
 #vmp_amp AS 
 #(SELECT DISTINCT id,
 #                 nm,
-#                 bnf_code,
-#                 udfs
+#                 bnf_code
 #FROM dmd.vmp #vmp table
 #WHERE bnf_code IS NOT NULL
 #UNION DISTINCT
 #SELECT DISTINCT vmp,
 #                nm,
-#                bnf_code,
-#                udfs
+#                bnf_code
 #FROM dmd.amp #amp table
 #WHERE bnf_code IS NOT NULL)
-
-#,
-ing_level_rx AS
-(select distinct ##distinct needed due to case statement for simple formulation
-rx.bnf_code,
-vmp.nm as name,
-CASE
-    WHEN vpi.strnt_nmrtr_uom = 258685003 THEN vpi.strnt_nmrtr_val / 1000 #creates miligram value from mcg value (uom = 258685003)
-    WHEN vpi.strnt_nmrtr_uom = 258682000 THEN vpi.strnt_nmrtr_val * 1000 #creates miligram value from gram value (uom = 258682000)
-    WHEN vpi.strnt_nmrtr_uom = 258684004 THEN vpi.strnt_nmrtr_val #normal miligram value (uom = 258684004)
-    ELSE null
-    END AS strnt_nmrtr_val_mg, #all listed drugs now in miligram rather than g or mcg
-CASE
-    WHEN vpi.strnt_dnmtr_uom = 258770004 THEN vpi.strnt_dnmtr_val * 1000 #some denominators listed as litre, so create mililitre value (uom = 258770004)
-    WHEN ing.id=373492002 AND form.descr LIKE '%transdermal%' THEN COALESCE(vpi.strnt_dnmtr_val/72, 1/72) # creates 72 hour dose for fentanyl transdermal patches, as doses are per hour on DM+D)
-    WHEN ing.id=387173000 AND form.descr LIKE '%transdermal%' AND strnt_nmrtr_val IN (5, 10, 15, 20) THEN COALESCE(vpi.strnt_dnmtr_val/168, 1/168) # creates 168 hour (7 day) dose for low-dose buprenorphine patch
-    WHEN ing.id=387173000 AND form.descr LIKE '%transdermal%' AND strnt_nmrtr_val IN (35, 52.5, 70) THEN COALESCE(vpi.strnt_dnmtr_val/96, 1/96) # creates 96 hour dose for higher-dose buprenorphine patch
-    ELSE vpi.strnt_dnmtr_val
-    END AS strnt_dnmtr_val_ml, #denominator now in ml
-ing.id as ing, 
-ing.nm as ing_name,
-COALESCE(udfs, 1) AS unit_dose,
-CASE 
-    WHEN form.descr LIKE '%injection%' THEN 'injection' #creates "injection" as route, regardless of whether injection or infusion. this also removes injection routes, e.g.
-    WHEN descr LIKE '%infusion%' THEN 'injection'  #s/c, i/v etc, AS often injections have many licensed routes, which would multiply the row
-        ELSE SUBSTR(form.descr, STRPOS(form.descr,".")+1) #takes the dosage form out of the string (e.g. tablet.oral) TO leave route.
-    END AS simple_form 
-from `ebmdatalab.richard.all_bnf_rx` as rx
-INNER JOIN
-dmd.vmp as vmp
-ON CONCAT(SUBSTR(rx.bnf_code,0,9),'AA',SUBSTR(rx.bnf_code,-2,2)) = CONCAT(SUBSTR(vmp.bnf_code,0,11),SUBSTR(vmp.bnf_code,-2,2) )
-INNER JOIN 
-dmd.vpi as vpi
-ON
-vmp.id = vpi.vmp
-INNER JOIN 
-dmd.ing as ing
-on
-vpi.ing = ing.id
-INNER JOIN
-dmd.ont as ont
-ON
-vmp.id = ont.vmp
-INNER JOIN 
-dmd.ontformroute as form
-ON
-ont.form = form.cd
-)
-
-select 
-#month, 
-name, 
-bnf_code,
-unit_dose,
-round(strnt_nmrtr_val_mg/coalesce(strnt_dnmtr_val_ml,1),3) as dose_per_unit,
-round(SUM(ome*strnt_nmrtr_val_mg/coalesce(strnt_dnmtr_val_ml, 1)),2) as total_ome#all other products have usual dose - coalesce as solid dose forms do not have a denominator),2) as total_ome
-from ing_level_rx as ing_rx
-inner join richard.opioid_class as opioid
-on
-ing_rx.ing = opioid.id and ing_rx.simple_form = opioid.form
-group by 
-#month, 
-name, bnf_code, ome, dose_per_unit, unit_dose
+    
+#main query to calculate the OME
+SELECT vpi.strnt_dnmtr_val_ml,
+       sum(rx.quantity) as quantity,
+       vmp.unit_dose_uom, 
+       vmp.udfs, 
+       ing.id, #ingredient DM+D code. Combination products will have more than one ing code per VMP, e.g. co-codamol will have ing for paracetamoland codeine
+       bnf.presentation as bnf_name, #ingredient name vmp.bnf_code AS bnf_code,
+       #vmp.nm AS vmp_nm, #VMP code
+       rx.bnf_code as bnf_code, #BNF code to link to prescribing data
+       vpi.strnt_nmrtr_val_mg, #strength numerator in mg
+       SUM(quantity*ome*(CASE
+           WHEN ing.id=373492002 AND form.simple_form = 'transdermal' THEN (vpi.strnt_nmrtr_val_mg*72)/coalesce(vpi.strnt_dnmtr_val_ml, 1) # creates 72 hour dose for fentanyl transdermal patches, as doses are per hour on DM+D)
+           WHEN ing.id=387173000 AND form.simple_form = 'transdermal' AND vpi.strnt_nmrtr_val IN (5, 10, 15, 20) THEN (vpi.strnt_nmrtr_val_mg*168)/coalesce(vpi.strnt_dnmtr_val_ml, 1) # creates 168 hour (7 day) dose for low-dose buprenorphine patch
+           WHEN ing.id=387173000 AND form.simple_form = 'transdermal' AND vpi.strnt_nmrtr_val IN (35, 52.5, 70) THEN (vpi.strnt_nmrtr_val_mg*96)/coalesce(vpi.strnt_dnmtr_val_ml, 1) # creates 96 hour dose for higher-dose buprenorphine patch
+           WHEN vmp.unit_dose_uom IN (413516001, 3318611000001103, 415818006) THEN (vmp.udfs*strnt_nmrtr_val_mg)/coalesce(vpi.strnt_dnmtr_val_ml, 1) # include unit dose size for ampoule, pre-filled injection and vial
+           ELSE strnt_nmrtr_val_mg/coalesce(vpi.strnt_dnmtr_val_ml, 1) #all other products have usual dose - coalesce as solid dose forms do not have a denominator
+       END)) AS total_ome, 
+       opioid.ome AS ome
+FROM norm_vpi AS vpi #VPI has both ING and VMP codes in the table
+INNER JOIN dmd.ing AS ing ON vpi.ing=ing.id #join to ING to get ING codes and name
+INNER JOIN dmd.vmp AS vmp ON vpi.vmp=vmp.id #join to get BNF codes for both VMPs and AMPs joined indirectly TO ING. 
+INNER JOIN simp_form AS form ON vmp.id=form.vmp #join to subquery for simplified administration route
+INNER JOIN richard.opioid_class_old AS opioid ON opioid.id=ing.id AND opioid.form=form.simple_form #join to OME table, which has OME value for ING/route pairs
+#INNER JOIN hscic.normalised_prescribing AS rx ON rx.bnf_code = vmp.bnf_code
+INNER JOIN hscic.normalised_prescribing AS rx ON CONCAT(SUBSTR(rx.bnf_code,0,9),'AA',SUBSTR(rx.bnf_code,-2,2)) = CONCAT(SUBSTR(vmp.bnf_code,0,11),SUBSTR(vmp.bnf_code,-2,2))
+INNER JOIN hscic.bnf as bnf ON rx.bnf_code = bnf.presentation_code
+WHERE rx.bnf_code NOT LIKE '0410%' #remove drugs used in opiate dependence
+and month between '2020-04-01' and '2021-03-01'
+#and rx.bnf_code LIKE '040702040AAABAB'
+GROUP BY id,
+         ing.nm,
+         rx.bnf_code,
+         bnf.presentation,
+         vmp.udfs,
+         vpi.strnt_nmrtr_val,
+         strnt_nmrtr_val_mg,
+         vpi.strnt_dnmtr_val_ml,
+         opioid.ome,
+         vmp.unit_dose_uom
 '''
-df_opioid_new = bq.cached_read(sql, csv_path=os.path.join('..','data','df_opioid_new_test.csv'))
+exportfile = os.path.join("..","data","df_opioid_new.csv") #set path for data cache
+df_opioid_new = bq.cached_read(sql, csv_path=exportfile, use_cache=False) #save dataframe to csv
 
 df_opioid_new.head(10)
 
 df_opioid_old.head(20)
 
 # +
-#df_opioid_new['dose_per_unit','total_ome'] = pd.to_numeric(df_opioid_new['dose_per_unit','total_ome'])
-#df_opioid_old["month"] = pd.to_datetime(df_opioid_old["month"]) 
-#df_opioid_new["month"] = pd.to_datetime(df_opioid_new["month"])
-#df_opioid_new[['quantity','dose_per_unit','total_ome']] = df_opioid_new[['quantity','dose_per_unit','total_ome']].apply(pd.to_numeric, axis = 1)
-#df_opioid_old[['quantity','dose_per_unit','total_ome']] = df_opioid_old[['quantity','dose_per_unit','total_ome']].apply(pd.to_numeric, axis = 1)
+df_opioid_new = df_opioid_new.drop(columns=['strnt_dnmtr_val_ml','unit_dose_uom','udfs','id','strnt_nmrtr_val_mg','ome'])
+
+
 # -
 
-pd.concat([df_opioid_old,df_opioid_new]).drop_duplicates(keep=False)
+def dataframe_difference(df1, df2, which=None):
+    """Find rows which are different between two DataFrames."""
+    comparison_df = df1.merge(
+        df2,
+        indicator=True,
+        how='outer'
+    )
+    if which is None:
+        diff_df = comparison_df[comparison_df['_merge'] != 'both']
+    else:
+        diff_df = comparison_df[comparison_df['_merge'] == which]
+    #diff_df.to_csv('data/diff.csv')
+    return diff_df
 
-new_df = pd.merge(df_opioid_old,df_opioid_new,  how='outer', on=['bnf_code'])
 
-new_df['ome_diff']=round(new_df['total_ome']-new_df['ome_dose'],1)
+dataframe_difference(df_opioid_new, df_opioid_old) 
 
-new_df['ome_diff_multi']=new_df['total_ome']/new_df['ome_dose']
+comparison_df.head(200)
+
+new_df.reset_index()
+
+new_df['ome_diff']=round(new_df['total_ome']-new_df['total_ome'],0)
+
+new_df['ome_diff_multi']=new_df['total_ome']/new_df['total_ome']
 
 new_df.to_csv('differences.csv')
 
